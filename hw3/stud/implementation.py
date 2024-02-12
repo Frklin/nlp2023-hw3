42c2824aca164fce7999e9af6913fb95c4d25b7d
Frklin <palandra.1849712@studenti.uniroma1.it>
2024-02-12 19:03:57 +0100

    code documentation

--- hw3/stud/implementation.py
+++ hw3/stud/implementation.py
@@ -8,7 +8,7 @@ from transformers import BertTokenizerFast
 from UniRel import UniRE
 from transformers import BertConfig
 from model import Model
-from load import set_position_shift, RelationDataset
+from load import set_position_shift
 import config
 import torch
 
@@ -43,10 +43,9 @@ class Baseline(Model):
 
 class StudentModel(Model):
 
-    # STUDENT: construct here your model
-    # this class should be loading your weights and vocabulary
 
     def __init__(self):
+        # Load the tokenizer
         self.tokenizer = BertTokenizerFast.from_pretrained(config.PRETRAINED_MODEL, do_basic_tokenize=True)
         self.rel2pred_str = " ".join([rel for rel in config.rel2pred.values()])
         self.encoded_preds = self.tokenizer.encode_plus(self.rel2pred_str, add_special_tokens=False)
@@ -54,31 +53,44 @@ class StudentModel(Model):
         self.bert_config.num_rels = config.REL_NUM
         self.bert_config.num_labels = config.REL_NUM
         self.bert_config.threshold = config.THRESHOLD
-        # self.is_additional_att = False
-        # self.is_separate_ablation = False
-        # self.test_data_type = False
+        
+        # Load the model
         self.model = UniRE.load_from_checkpoint(config.LOAD_MODEL_PATH, config=self.bert_config)
         self.model.eval()
-        # self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        # self.model.to(self.device)
 
     def predict(self, tokens: List[List[str]]) -> List[List[Dict]]:
-
+        
+        # Get the maximum length of the tokens for the padding
         max_len = max([len(self.tokenizer.encode(" ".join(sentence))) for sentence in tokens])
 
+        # Get the input_ids, attention_masks, token_type_ids
         indices, input_ids, attention_masks, token_type_ids = self.get_input_data(tokens, max_len)       
 
         input_data = [indices, input_ids, attention_masks, token_type_ids]
 
+        # Get the predictions
         with torch.no_grad():
             preds = self.model.predict(input_data, max_len)
 
         return preds
 
-    def get_input_data(self, tokens, max_len):
+    def get_input_data(self, tokens: List[List[str]], max_len: int) -> List[torch.Tensor]:
+        """
+        Get the input_ids, attention_masks, token_type_ids
+        
+        Args:
+            tokens: List of tokens
+            max_len: Maximum length of the tokens for the padding
+
+        Returns:
+            indices: List of indices
+            input_ids: List of input_ids
+            attention_masks: List of attention_masks
+            token_type_ids: List of token_type_ids
+        """
+
         indices = []
         input_ids = []
-        position_ids = []
         attention_masks = []
         token_type_ids = []
         config.index_shift = {}
@@ -86,26 +98,28 @@ class StudentModel(Model):
         for idx, sentence in enumerate(tokens):
             sentence_input_ids = [self.tokenizer.cls_token_id]
             sentence_position_ids = [-1]
+
+            # Ecode the tokens and keep track of the position ids
             for i in range(len(sentence)):
                 encoded_token = self.tokenizer.encode(sentence[i], add_special_tokens=False)
                 sentence_input_ids.extend(encoded_token)
                 sentence_position_ids.extend([i] * len(encoded_token))
 
-
-            # PADDING
+            # Pad the input_ids, attention_masks, token_type_ids
             sentence_attention_masks = [1] * len(sentence_input_ids) + [0] * (max_len - len(sentence_input_ids)) + [1] * config.REL_NUM
             sentence_token_type_ids = [0] * len(sentence_input_ids) + [0] * (max_len - len(sentence_input_ids)) + [1] * config.REL_NUM
             sentence_input_ids += [0] * (max_len - len(sentence_input_ids)) + self.encoded_preds['input_ids']
             sentence_position_ids += [-1] * (len(sentence_input_ids) - len(sentence_position_ids))
 
+            # Set the position shift to keep track of the position of the tokens
             set_position_shift(idx, sentence_position_ids)
-
 
             indices.append(idx)
             input_ids.append(torch.tensor(sentence_input_ids))
             attention_masks.append(torch.tensor(sentence_attention_masks))
             token_type_ids.append(torch.tensor(sentence_token_type_ids))
 
+        # Turn the lists into tensors
         input_ids = torch.stack(input_ids)
         attention_masks = torch.stack(attention_masks)
         token_type_ids = torch.stack(token_type_ids)
@@ -113,24 +127,9 @@ class StudentModel(Model):
         return indices, input_ids, attention_masks, token_type_ids
 
     def to(self, device: str):
+        """
+        Move the model to the device
+        """
         self.model.to(device)
         return self
 
-
-if __name__ == "__main__":
-    first_sentence = ["In", "baseball", ",", "if", "Tom", "Glavine", "shuts", "out", "Atlanta", "for", "the", "Mets", "on", "Monday", "and", "the", "Braves", "come", "back", "the", "next", "day", "and", "knock", "John", "Maine", "out", "of", "the", "game", "in", "the", "first", "inning", "and", "go", "on", "to", "rout", "the", "Mets", ",", "Glavine", "'s", "shutout", "is", "n't", "wiped", "out", "."]
-    second_sentence = ["But", "lawmakers", "in", "those", "districts", ",", "including", "Representative", "Roy", "Blunt", "of", "Missouri", ",", "the", "third-ranking", "Republican", "in", "the", "House", ",", "were", "not", "told", "about", "the", "poll", "and", "were", "caught", "off", "guard", "by", "it", "."]
-
-    first_sentence_labels = [{"subject": {"start_idx": 4, "end_idx": 6, "entity_type": "PERSON", "text": "Tom Glavine"}, "relation": "/people/person/place_lived", "object": {"start_idx": 8, "end_idx": 9, "entity_type": "LOCATION", "text": "Atlanta"}}]
-    second_sentence_labels =[{"subject": {"start_idx": 8, "end_idx": 10, "entity_type": "PERSON", "text": "Roy Blunt"}, "relation": "/people/person/place_lived", "object": {"start_idx": 11, "end_idx": 12, "entity_type": "LOCATION", "text": "Missouri"}}]
-
-    tokenizer = BertTokenizerFast.from_pretrained(config.PRETRAINED_MODEL, do_basic_tokenize=True)
-
-    # tokens = [first_sentence, second_sentence]
-    test_data = RelationDataset(config.TEST_PATH, tokenizer)
-
-
-    model = build_model("cpu")
-
-    for i in range(0, len(test_data.tokens), 16):
-        print(model.predict(test_data.tokens[i:i+16]))
